17bac98c291dc2b5a7b9bba6a2aea4670d7441fa
Rashad Philizaire <rashadp@fb.com>
2022-06-27 20:17:53 -0700

    changed permissions
    Signed-off-by: Rashad Philizaire <rashadp@fb.com>

---
+++ Instagram-Clone/ComposeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ComposeViewController.h
+//  Instagram-Clone
+//
+//  Created by Rashad Philizaire on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
